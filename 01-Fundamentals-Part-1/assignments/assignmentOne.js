55ecbe0224cfe26dfd694bb0cce9abfb44875e84
Zack Bumpous <upwork.ztb@gmail.com>
2021-02-13 18:47:44 -0600

    Completes assignment one by adding a new directory containing a .js file that declares variables, stores info in these variables, and then logs them to the console.

---
+++ 01-Fundamentals-Part-1/assignments/assignmentOne.js
@@ -0,0 +1,8 @@
+let country, continent, population;
+country = "United States";
+continent = "North America";
+population = "300 some odd million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let js = "amazing";
+if (js === 'amazing') alert("JavaScript is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Zack";
+console.log(firstName);
