266f47d91f372aff46468bc235ca3314fa3f7de6
Zack Bumpous <upwork.ztb@gmail.com>
2021-02-15 11:57:01 -0600

    Adds code following along to lecture into script.js and creates a file inside assignments directory, this file follows along to assignment 2 in the course.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,23 @@
-let js = "amazing";
-if (js === 'amazing') alert("JavaScript is FUN!");
+// 1.
+// let js = "amazing";
+// if (js === 'amazing') alert("JavaScript is FUN!");
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = "Zack";
-console.log(firstName);
+// let firstName = "Zack";
+// console.log(firstName);
+
+
+
+
+
+// 2.
+// let jsIsFun = true;
+// console.log(jsIsFun);
+// console.log(typeof true);
+
+
+
+
+
+// 3.
