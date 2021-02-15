4d4fd9da6bdf66b7689e189631cfaff901c07099
Zack Bumpous <upwork.ztb@gmail.com>
2021-02-15 13:34:54 -0600

    Adds follow along code to the Basic Operators lecture and adds code that finishes the corresponding assignment.

---
+++ 01-Fundamentals-Part-1/assignments/assignmentFour.js
@@ -0,0 +1,13 @@
+let country;
+
+country = 350;
+halfCountry = country / 2; // 175
+country++; // 351
+console.log("Country (350) ++ equals: ", country);
+countryFinland = 6;
+console.log("My country has more people than Finland does. This statement is: ", country > countryFinland);
+countryAverage = 33;
+console.log("My country has more people than the average does. This statement is: ", country > countryAverage);
+
+const description;
+description = 'Portugal is in Europe, and its 11 million people speak Portuguese';
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -22,12 +22,47 @@
 
 // 3.
 // mutating a variable with let
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-// keeping a variable constant with const
-const birthYear = 1990;
-// birthYear = 1991; will not work
+// // keeping a variable constant with const
+// const birthYear = 1990;
+// // birthYear = 1991; will not work
 
-var job = 'programmer';
-job = 'teacher'; // looks like var and let are the same, but they are different
+// var job = 'programmer';
+// job = 'teacher'; // looks like var and let are the same, but they are different
+
+
+
+
+
+
+// 4.
+let year = 2021;
+const ageSomeone = year - 1996;
+const ageSomeoneElse = year - 2010;
+console.log(ageSomeone, ageSomeoneElse);
+console.log(ageSomeoneElse * 2, ageSomeone * 2);
+console.log(2 ** 3); // 2 to the power of 3 (2 * 2 * 2)
+
+// we can use the + operator to combine/concatenate strings
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; // x = 15;
+console.log("X equals: ", x);
+x += 10 // x = x + 10 (x now === 25)
+console.log("X equals: ", x);
+x *= 4;
+console.log("X equals: ", x);
+x++;
+console.log("X equals: ", x);
+x--;
+console.log("X equals: ", x);
+
+
+// Comparison operators
+console.log(ageSomeone > ageSomeoneElse); // true
+const isFullAge = ageSomeone > 18;
