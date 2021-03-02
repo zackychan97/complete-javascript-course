facef6bc0d3456f3b45b1bd6122dcca569190298
Zack Bumpous <upwork.ztb@gmail.com>
2021-03-02 17:02:51 -0600

    Adds code following along to the Activating Strict Mode lecture.

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,21 @@
+// Activating Strict Mode
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// use strict allows us to see in console that hasDriverLicense isn't defined
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense){
+    console.log(`I can drive :D`);
+}
+
+// strict mode adds certain features JS may implement in the future. interface is now reserved word under strict mode
+const interface = 'Audio';
+
+
+
+
+
+
+// Functions
