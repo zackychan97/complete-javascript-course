913bcbeb6869721fcb93970f55cfcadf5d99a828
Zack Bumpous <upwork.ztb@gmail.com>
2021-03-08 10:29:01 -0600

    Creates a couple of arrow functions following along to lecture code for Arrow Functions lecture.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -52,22 +52,56 @@
 
 // Function Declarations vs Expressions
 // Declaration below
-function calcAgeDeclaration(birthYear){
-    // const age = 2037 - birthYear;
-    // return age;
-    // or...
-    return 2037 - birthYear;
-}
+// function calcAgeDeclaration(birthYear){
+//     // const age = 2037 - birthYear;
+//     // return age;
+//     // or...
+//     return 2037 - birthYear;
+// }
 
-const age1 = calcAgeDeclaration(1997);
-console.log(age1);
+// const age1 = calcAgeDeclaration(1997);
+// console.log(age1);
 
 
-// function expression below
-// also called anonymous function (bc no function name)
+// // function expression below
+// // also called anonymous function (bc no function name)
+// const calcAgeExpression = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAgeExpression(1997);
+
+// console.log(age1, age2);
+
+
+
+
+
+
+
+// Arrow Functions
+
+// Function Expression below
 const calcAgeExpression = function (birthYear) {
     return 2037 - birthYear;
 }
-const age2 = calcAgeExpression(1997);
 
-console.log(age1, age2);
+// Arrow Function below (doesn't need to be assigned to variable)
+const calcAgeArrow = birthYear => 2037 - birthYear;
+const ageArrowFunction = calcAgeArrow(1997);
+console.log(ageArrowFunction);
+
+const yearsUntilRetirement = birthYear => {
+    const age = 2021 - birthYear;
+    const retirement = 65 - age;
+    return `${retirement} years left`;
+}
+
+console.log(yearsUntilRetirement(1991));
+
+
+const fruitBlender = (apples, oranges) => {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges. 🥤`;
+    return juice; // we will return the juice data/info/value
+}
+
+console.log(fruitBlender(5, 4));
