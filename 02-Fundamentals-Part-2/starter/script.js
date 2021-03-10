496bd33d2d0684f7bdc0e017baca35d62f0b71ec
Zack Bumpous <upwork.ztb@gmail.com>
2021-03-10 17:13:50 -0600

    Adds follow along code to lecture Introduction to Arrays. Demonstrates creating an array and accessing its values, as well as mutating the array.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -161,5 +161,45 @@
 
 
 
+// Introduction to Arrays
 
+const friend1 = "Michael";
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]); 
+console.log(friends.length); //.length is a property of arrays
+console.log(friends.length - 1) // returns last element of an array
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const jonas = ['Jonas', 'Schmedtmann', 2021 - 1991];
+
+const firstName = 'Jonas';
+const jonas2 = [firstName, 'Schmedtmann', 2021 - 1991, 'teacher', friends];
+
+console.log(jonas);
+console.log(jonas2);
+
+// Exercise
+const calcAge = function(birthYear){
+    return (2021 - birthYear);
+}
+const yearsArray = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(yearsArray[0]);
+const age2 = calcAge(yearsArray[1]);
+const age3 = calcAge(yearsArray[2]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(yearsArray[0]), calcAge(yearsArray[1]), calcAge(yearsArray[2]), calcAge(yearsArray[3])];
+console.log(ages);
 
