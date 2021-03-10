9a6c9765e5e93bf227d7fd2f877973ec90eb19fc
Zack Bumpous <upwork.ztb@gmail.com>
2021-03-09 19:43:12 -0600

    Completes Section 2 Challenge 1. Creates two functions, one is an arrow function that calculates the average of 3 numbers. The second is a function expression that will take in the averages of a couple teams and their team names and console log the winner based on averages.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -134,22 +134,55 @@
 
 // Reviewing Functions
 
-const calcAge = function(birthYear){
-    return 2021 - birthYear;
-}
+// const calcAge = function(birthYear){
+//     return 2021 - birthYear;
+// }
 
-const yearsUntilRetirement = function (birthYear, firstName){
-    const age = calcAge(birthYear)
-    const retirement = 65 - age;
+// const yearsUntilRetirement = function (birthYear, firstName){
+//     const age = calcAge(birthYear)
+//     const retirement = 65 - age;
 
-    if (retirement > 0){
-        console.log(`${firstName} has ${retirement} years left`);
-        return retirement;
-    } else {
-        console.log(`${firstName} has already retired!`);
-        return -1;
+//     if (retirement > 0){
+//         console.log(`${firstName} has ${retirement} years left`);
+//         return retirement;
+//     } else {
+//         console.log(`${firstName} has already retired!`);
+//         return -1;
+//     }
+//     }
+
+//     console.log(yearsUntilRetirement(1997, 'Zack'));
+//     console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+
+
+
+
+
+
+    const calcAverage = (score1, score2, score3) => {
+        const average = (score1 + score2 + score3) / 3;
+        return average;
     }
-    }
+    
+    const dolphinScore = calcAverage(44, 23, 71);
+    const koalaScore = calcAverage(65, 54, 49);
 
-    console.log(yearsUntilRetirement(1997, 'Zack'));
-    console.log(yearsUntilRetirement(1950, 'Mike'));
+    const dolphinScore2 = calcAverage(85, 54, 41);
+    const koalaScore2 = calcAverage(23, 34, 27);
+
+    const checkWinner = function(team1, team2, team1Name, team2Name){
+        if (team1 >= (team2 * 2)){
+            console.log(`${team1Name} wins averaging ${team1} vs ${team2}!!`);
+        } else if (team2 >= (team1 * 2)){
+            console.log(`${team2Name} wins averaging ${team2} vs ${team1}!!`)
+        } else {
+            console.log(`Either a draw or error.`)
+        }
+    }
+    
+    
+    checkWinner(dolphinScore, koalaScore, 'Dolphins', 'Koalas');
+    checkWinner(dolphinScore2, koalaScore2, 'Dolphins', 'Koalas');
+
