d386d08cdb3f794d12e066ce3ee7b44657a859ef
Zack Bumpous <upwork.ztb@gmail.com>
2021-03-29 14:32:15 -0500

    Set up prettier inside section 03.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const y = '23';
+const x = 23;
+if (x === 23) {
+  console.log(23);
+}
+
+const calcAge = birthYear => 2021 - birthYear;
+
+console.log("hello world");
+
