b0b4a687b520a411d8c4a4c52142a624f0ff8b6c
Zack Bumpous <upwork.ztb@gmail.com>
2021-03-29 15:13:08 -0500

    Creates a function that will accept 2 arrays as arguments, each array representing a list of numbers that are representing temperatures. Function created then concats these arrays into one, and, then loops over them, finding both the MAX and MIN temperature in the array. Function then returns the amplitude.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,13 +1,47 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const y = '23';
-const x = 23;
-if (x === 23) {
-  console.log(23);
-}
+// const y = '23';
+// const x = 23;
+// if (x === 23) {
+//   console.log(23);
+// }
 
-const calcAge = birthYear => 2021 - birthYear;
+// const calcAge = birthYear => 2021 - birthYear;
 
-console.log("hello world");
+// console.log("hello world");
 
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [ 5, 6, 'error', 102];
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  // Combine the two arrays
+  const temps = t1.concat(t2);
+
+
+  
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    // If we encounter 'error':
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') {
+      continue;
+    }
+
+    // Find MAX:
+    if (temps[i] > max) {
+      max = temps[i];
+    }
+    // Find MIN:
+    if (temps[i] < min) {
+      min = temps[i];
+    }
+  }
+  console.log(max, min);
+  return (max - min);
+};
+
+calcTempAmplitudeNew([24, 36, 7, 90], [4, 3, 2, 99]);
+const amplitude = calcTempAmplitudeNew(temperatures, temperatures2);
+console.log(amplitude);
