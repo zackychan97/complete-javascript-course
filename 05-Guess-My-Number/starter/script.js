2517098c66a26bf44914389d5ffbb67fd1798dd2
Zachary <upwork.ztb@gmail.com>
2021-04-01 17:21:57 -0500

    Creates two similar functions. Functions each take 1 param and assign that to the value of the document respective className via query selector

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,43 +12,44 @@ let score = 20;
 // High Score tracker defined in global scope
 let highScore = 0;
 
+// Display Message Query Selector, a Function that takes a message as a parameter and sets our message text to that parameter input to function
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+// Function that will use query selector to change textContent of our Current Score displayed with the param we pass in
+const displayScore = function (message) {
+  document.querySelector('.score').textContent = message;
+};
+
 // An event listener (click) that contains our control flow for handling our guesses inside of it.
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   // Control Flow for handling our guesses below
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️  No Number!';
+    // document.querySelector('.message').textContent = '⛔️  No Number!';
+    displayMessage('⛔️  No Number!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    // document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
     // Updating High Score if highscore > current high score, only updating WHEN/IF we win
-    if (score > highScore){
+    if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High!';
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-
-      document.querySelector('body').style.backgroundColor = '#FF0000';
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
+      displayScore(0);
       document.querySelector('body').style.backgroundColor = '#FF0000';
     }
   }
@@ -56,7 +57,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  document.querySelector('.score').textContent = score;
+  displayScore(score);
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
